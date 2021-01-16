ca68c6e0553cbd55e614a000782b018e8148e7c5
Laotang1234 <tangfengpo@163.com>
2021-01-16 17:09:32 +0800

    完成文章列表和添加文章页面的布局

---
+++ article/js/add.js
@@ -0,0 +1,14 @@
+initEditor(); // 调用函数，就会把 textarea 替换为富文本编辑器
+
+// 1. 初始化剪裁框
+// 1. 初始化图片裁剪器
+var $image = $('#image')
+
+// 2. 裁剪选项
+var options = {
+    aspectRatio: 400 / 280,
+    preview: '.img-preview'
+}
+
+// 3. 初始化裁剪区域
+$image.cropper(options)

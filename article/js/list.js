439c7f2c5f661bc13b24647d83c77be7dba72751
Laotang1234 <tangfengpo@163.com>
2021-01-17 10:28:43 +0800

    完成分页及筛选的下拉框

--- article/js/list.js
+++ article/js/list.js
@@ -47,7 +47,43 @@ function showPage(t) {
     count: t, // 数据总数，从服务端得到
     limit: data.pagesize, // 每页显示几条数据
     curr: data.pagenum, // 当前页，比如3，效果就是让第3页背景绿色，表示选中
+    limits: [2, 3, 5, 10], // 下拉框中的条数
+
+    layout: ['limit', 'prev', 'page', 'next', 'count', 'skip'], // 自定义排版
+
+    // 分页的回调（showPage调用时触发第1次；后续点击页码还会触发）
+    jump: function (obj, first) {
+      //obj包含了当前分页的所有参数，比如：
+      // console.log(obj.curr); // 得到当前页，以便向服务端请求对应页的数据。
+      // console.log(obj.limit); // 得到每页显示的条数
+      // 首次不执行
+      if (!first) {
+        // do something
+        // 修改当前页码
+        data.pagenum = obj.curr; // 比如点了3，obj.curr=3; 把3赋值给ajax请求参数
+        // 修改每页显示的条数
+        data.pagesize = obj.limit; // 比如修改了每页显示5条，及时更新ajax请求参数
+        renderArticle();
+        // console.log('hello')
+      }
+    }
   });
 }
 
+
+// -------------------------- 筛选 ------------------------------
+var form = layui.form;
+// 1. 获取真实的分类，渲染到下拉框的位置
+$.ajax({
+  url: '/my/category/list',
+  success: function (res) {
+    // console.log(res)
+    var str = template('tpl-category', res);
+    // $('#category').append(str);
+    $('#category').html(str);
+    // 更新渲染
+    // form.render('select', 'lay-filter属性值');
+    form.render('select');
+  }
+})
 
